6229f2abb82f476c79de2cf968a900d8006f6e89
vaidehibhagwat <vaidehibhagwat225@gmail.com>
2021-11-22 14:12:22 +0530

    UPDATE:Refactored tests for AuthRepository

--- test/setup/constants.dart
+++ test/setup/constants.dart
@@ -3,5 +3,8 @@ import 'package:everythng/domain/auth/entities/everythng_user.dart';
 const email = "user@everythng.com";
 const password = "skjfasj.ka129!@";
 const uid = "UID";
+const String token = 'TOKEN';
+const String refreshedToken = 'REFRESHEDTOKEN';
+const String url = 'http://www.everythng.in';
 
 const user = EverythngUser(email: email, uid: uid);
