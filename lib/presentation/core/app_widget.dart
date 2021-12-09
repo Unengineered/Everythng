b5d64ac5fe22c1d1c92c73fc0aabe0de24cbd21a
Kapish26 <kapishmadhwani@gmail.com>
2021-12-09 12:50:38 +0530

    FEATURE: Started with responsive design.

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -5,6 +5,7 @@ import 'package:everythng/presentation/routes/auth_router.dart';
 import 'package:everythng/presentation/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class AppWidget extends StatelessWidget {
   final _appRouter = AppRouter();
@@ -21,6 +22,16 @@ class AppWidget extends StatelessWidget {
         child: AuthRouter(
           router: _appRouter,
           child: MaterialApp.router(
+            builder: (context , widget) =>  ResponsiveWrapper.builder(
+              BouncingScrollWrapper.builder(context, widget!),
+              // maxWidth: 1200,
+              minWidth: 350,
+              defaultScale: true,
+              breakpoints: const [
+                ResponsiveBreakpoint.autoScale(350, name: MOBILE),
+                ResponsiveBreakpoint.autoScale(600, name: TABLET),
+              ],
+            ),
             title: 'Everythng App',
             debugShowCheckedModeBanner: false,
             theme: ThemeData(
