a691b057050215bc47a990a4e8c2d1231d70a3a6
advait <advaitbansode4@gmail.com>
2021-12-14 17:57:57 +0530

    FEATURE: removed bugs from profile system and completed checks with the backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 
+import 'dart:developer';
+
 import 'package:everythng/presentation/core/app_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fort_knox/fort_knox.dart';
 
 import 'injection.dart';
 
@@ -9,6 +12,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   configureDependencies();
-  //log((await getIt<FortKnox>().getToken()).toString());
+  log((await getIt<FortKnox>().getToken()).toString());
   runApp(AppWidget());
 }
